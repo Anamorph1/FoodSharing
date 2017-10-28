0504685c53612c87421e39398e06f49b2cc088e5
slivka- <slivka104@gmail.com>
2017-10-28 22:42:38 +0200

    Fixed image uris

--- FoodSharing/API/Model/StaticResources/ProductImages.cs
+++ FoodSharing/API/Model/StaticResources/ProductImages.cs
@@ -9,10 +9,11 @@ namespace API.Model.StaticResources
     {
         public static readonly List<Uri> ImageUris = new List<Uri>()
         {
-            new Uri(@"https://lh6.googleusercontent.com/rmBCpGPyrv_mPY3aDc3fpXvms8W-tfYd75vsaFSrBE76tNTXQL0dGIErJ9vWVDskSaFkxovWWr9PJy8=w1366-h662"),
-            new Uri(@"https://lh3.googleusercontent.com/1tbK_OfbnR5FFYoZCn0LO2v3wUCd1HF-XAXsFJ60vB5DaNTtPaMhN2UMPr2Q279erBorTD1F7ry2CUk=w1366-h662"),
-            new Uri(@"https://lh3.googleusercontent.com/og55Z3gVexGeNH7cC7a5TDePpMtR2U3YbqWdDqK9lwjtDfFi5UNujZYi3mF5PrK9etH-CotGrYQbm6c=w1366-h662"),
-            new Uri(@"https://lh4.googleusercontent.com/h2jkdbVh0Dn_DX56Bc4FJ0VAyvOyXGpJH5QiE8dvztkb1NS5i0ksewGNyRKJLNPpHz71D0aJRLTlZvs=w1366-h662")
+            new Uri(@"http://www.mojegotowanie.pl/media/cache/default_view/uploads/media/default/0001/76/1f52b486f01de6f770d6b97cca1880a695376c2e.jpeg"),
+            new Uri(@"https://4.bp.blogspot.com/-3s4L2hp-IQM/U4154FTXZgI/AAAAAAAAi68/JtbVzCFAqcg/s1600/frytki+domowe+%25285%2529.JPG"),
+            new Uri(@"http://www.mojegotowanie.pl/media/cache/default_view/uploads/media/default/0001/55/9be3cacf21797f8a11cd16553962d4e6e2ad82d4.jpeg"),
+            new Uri(@"https://img.grouponcdn.com/deal/mbyUEW6TfTGgb28PJUor/fG-2048x1229/v1/c700x420.jpg"),
+            new Uri(@"https://www.kwestiasmaku.com/sites/kwestiasmaku.com/files/bigos_z_kiszonej_kapusty_01_0.jpg")
         };
         private static readonly int imageCount = ImageUris.Count;
 
