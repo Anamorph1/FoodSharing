01bbbfd0c77e940084a6c72843074f6a3994b82f
Wojtek Gumula <wgumula@student.agh.edu.pl>
2017-10-28 15:48:02 +0200

    Removed Client app. Created api root controller.

---
+++ FoodSharing/API/Controllers/RootController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace API.Controllers
+{
+    [Produces("application/json")]
+    [Route(Routes.API)]
+    public class RootController : Controller
+    {
+        // GET: api/Root
+        [HttpGet]
+        public Dictionary<string, string> Get()
+        {
+            var root = "http://" + Request.Host.ToString() + "/";
+            return Routes.GetRoutes(root);
+        }
+
+    }
+}
---
+++ FoodSharing/API/Routes.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API
+{ 
+    public class Routes
+    {
+        public const string API = "api/";
+        public const string POSTS = "posts/";
+        public const string POST_ID = POSTS + "{id}/";
+
+        public static Dictionary<string, string> GetRoutes(string root)
+        {
+            Dictionary<string, string> routes = new Dictionary<string, string>();
+
+            routes["root"] = root + API;
+            routes["posts"] = root + POSTS;
+            routes["posts_id"] = root + POST_ID;
+
+            return routes;
+        }
+    }
+}
