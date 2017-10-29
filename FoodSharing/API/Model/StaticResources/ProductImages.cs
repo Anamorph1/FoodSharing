437ac1fa443521363648d1af084df09deed9c25e
Wojtek Gumula <wgumula@student.agh.edu.pl>
2017-10-29 07:01:26 +0100

    https images

--- FoodSharing/API/Model/StaticResources/ProductImages.cs
+++ FoodSharing/API/Model/StaticResources/ProductImages.cs
@@ -9,10 +9,10 @@ namespace API.Model.StaticResources
     {
         public static readonly List<Uri> ImageUris = new List<Uri>()
         {
-            new Uri(@"http://www.mojegotowanie.pl/media/cache/default_view/uploads/media/default/0001/76/1f52b486f01de6f770d6b97cca1880a695376c2e.jpeg"),
             new Uri(@"https://4.bp.blogspot.com/-3s4L2hp-IQM/U4154FTXZgI/AAAAAAAAi68/JtbVzCFAqcg/s1600/frytki+domowe+%25285%2529.JPG"),
-            new Uri(@"http://www.mojegotowanie.pl/media/cache/default_view/uploads/media/default/0001/55/9be3cacf21797f8a11cd16553962d4e6e2ad82d4.jpeg"),
             new Uri(@"https://img.grouponcdn.com/deal/mbyUEW6TfTGgb28PJUor/fG-2048x1229/v1/c700x420.jpg"),
+            new Uri(@"https://eat24hours.com/files/cuisines/v4/thai.jpg"),
+            new Uri(@"https://media2.s-nbcnews.com/j/newscms/2017_10/1200234/10-healthy-fast-food-meals-008-subway-inline-today-170309_89a32509f1b93e969a831a913cc2a2d1.today-inline-large.jpg"),
             new Uri(@"https://www.kwestiasmaku.com/sites/kwestiasmaku.com/files/bigos_z_kiszonej_kapusty_01_0.jpg")
         };
         private static readonly int imageCount = ImageUris.Count;
